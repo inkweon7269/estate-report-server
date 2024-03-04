fd7cfe065c1e097d856ed3c0867157615377b94a
Inkwon Kim <inkweon7269@gmail.com>
2024-03-05 01:51:24 +0900

    Feat : 미들웨어 추가

--- src/resource/user/user.module.ts
+++ src/resource/user/user.module.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Module } from '@nestjs/common';
+import { BadRequestException, MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '@root/entities/user.entity';
 import { UserController } from '@root/resource/user/user.controller';
@@ -9,10 +9,19 @@ import { extname } from 'path';
 import * as multer from 'multer';
 import { USERS_IMAGE_PATH } from '@root/common/const/path.const';
 import { v4 as uuid } from 'uuid';
+import { LogMiddleware } from '@root/middleware/log.middleware';
 
 @Module({
     imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
     controllers: [UserController],
     providers: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+    configure(consumer: MiddlewareConsumer): any {
+        // 적용하고자 하는 미들웨어 추가
+        consumer.apply(LogMiddleware).forRoutes({
+            path: '*user*',
+            method: RequestMethod.ALL,
+        });
+    }
+}
