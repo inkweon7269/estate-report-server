e83801adbf6a41da03469f56359dc30e317a2e82
Inkwon Kim <inkweon7269@gmail.com>
2024-06-30 02:09:43 +0900

    feature : 페이지네이션 반환값 내용 추가

--- src/common/dtos/output.dto.ts
+++ src/common/dtos/output.dto.ts
@@ -4,23 +4,23 @@ export class PaginationResponse<T> {
     @ApiProperty({ isArray: true, description: '반환되는 목록' })
     list: T[];
 
-    @ApiProperty({ name: 'totalCount', description: '총 결과 갯수' })
+    @ApiProperty({ description: '전체 데이터 개수' })
     totalCount: number;
 
-    @ApiProperty()
-    limit: number;
-
-    @ApiProperty()
+    @ApiProperty({ description: '전체 페이지' })
     totalPage: number;
 
     @ApiProperty()
     page: number;
 
+    @ApiProperty()
+    limit: number;
+
     constructor(data: T[], totalCount: number, page: number, limit: number) {
         this.list = data;
         this.totalCount = totalCount;
-        this.limit = limit;
         this.totalPage = Math.ceil(totalCount / limit);
         this.page = page;
+        this.limit = limit;
     }
 }
